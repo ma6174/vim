001393afebb184c7e8d79f528e150b16f2fe3c4e
ma6174 <ma6174@ma6174.(none)>
2012-08-04 09:25:13 +0800

    delete some file

--- markdown.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-#coding=utf-8
-import markdown
-import sys
-if __name__=='__main__':
-    in_file,out_file = sys.argv[1:]
-    html = markdown.markdownFromFile(in_file,out_file,encoding='utf-8')
