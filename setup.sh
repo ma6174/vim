2ea334dd78fdd6aac1c51346d9c463228e633810
ma6174 <ma6174@163.com>
2013-05-31 08:49:54 +0800

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -9,5 +9,9 @@ mv ~/.vim ~/.vim_old
 mv ~/vim ~/.vim
 mv ~/.vim/.vimrc ~/
 git clone http://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
+echo "ma6174正在努力为您安装bundle程序" > ma6174
+echo "安装完毕将自动退出" >> ma6174
+echo "请耐心等待" >> ma6174
 vi ma6174 -c "BundleInstall" -c "q" -c "q"
+rm ma6174
 echo "安装完成"
