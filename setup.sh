c396c603e13f4af95c7032e78af287aad7ebe0f5
ma6174 <ma6174@163.com>
2013-05-31 08:39:10 +0800

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,7 +3,9 @@ echo "安装将花费一定时间，请耐心等待直到安装完成^_^"
 sudo apt-get install -y vim vim-gnome ctags xclip astyle python-setuptools
 sudo easy_install -ZU autopep8 twisted
 sudo ln -s /usr/bin/ctags /usr/local/bin/ctags
+mv ~/vim ~/vim_old
 cd ~/ && git clone git://github.com/ma6174/vim.git
+mv ~/.vim ~/.vim_old
 mv ~/vim ~/.vim
 mv ~/.vim/.vimrc ~/
 git clone http://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
