69f2704aa229fbccb8eab48dbafdd9ea5c6b48e8
ma6174 <ma617495@gmail.com>
2014-03-12 09:57:10 +0800

    Update setup.sh
    http -> https at github

--- setup.sh
+++ setup.sh
@@ -19,7 +19,7 @@ cd ~/ && git clone https://github.com/ma6174/vim.git
 mv -f ~/.vim ~/.vim_old
 mv -f ~/vim ~/.vim
 mv -f ~/.vim/.vimrc ~/
-git clone http://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
+git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
 echo "ma6174正在努力为您安装bundle程序" > ma6174
 echo "安装完毕将自动退出" >> ma6174
 echo "请耐心等待" >> ma6174
